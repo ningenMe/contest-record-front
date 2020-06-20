0bdc89ffda6ceb4f9bb51feaa8c86be4ce6b5f9a
ningenMe <amohkuru@gmail.com>
2020-06-20 13:29:53 +0900

    modify title

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ export default class Header extends React.Component {
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link target="_blank" href="https://ningenme.net/">ningenMe.net</Nav.Link>
+          {/* <Nav.Link target="_blank" href="https://ningenme.net/">ningenMe.net</Nav.Link> */}
           <NavDropdown title="GitHub" id="basic-nav-dropdown">
             <NavDropdown.Item target="_blank" href="https://github.com/ningenMe/contest-record-front">ui</NavDropdown.Item>
             <NavDropdown.Item target="_blank" href="https://github.com/ningenMe/ningenme-net-back">api</NavDropdown.Item>
