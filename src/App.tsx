d946dc4e7e04c09890af92c33601b0a3ef3e1c9c
ningenMe <amohkuru@gmail.com>
2021-01-02 17:38:08 +0900

    [add] footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './components/Header'
 import Router from './components/Router'
+import Footer from './components/Footer'
 
 export const App: React.FC = () => {
     return (
       <>
         <Header />
         <Router />
+        <Footer />
       </>
       );
     }
